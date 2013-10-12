673db409753b1d30d9f3788af670d98046dcdebe
Ryan Wong <ryanawong@gmail.com>
2013-10-12 00:35:01 -0400

    Initial commit (rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acme::Application.config.secret_key_base = '2207c90d715ec87e1dcf2853893c55be4758ad934136f37f6db22012feb89d3c77bf0332634f0da55beba483939424ff2989558d473ad7d04c9f99428f271147'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acme::Application.config.session_store :cookie_store, key: '_acme_session'
